d5d5594e85cc824f58ab2eefe509d235b762e88a
004Ajay <85185407+004Ajay@users.noreply.github.com>
2022-02-11 16:27:56 +0530

    change

--- Array2ndSmallest.java
+++ Array2ndSmallest.java
@@ -8,10 +8,25 @@ System.out.println("2nd smallest array element is " +array[1]);
 }
 
 /*
-public class arrays{
+Java program to get array from user & find the second smallest element of it.
+
+import java.util.Scanner;
+public class DefaultRough{
     public static void main(String args[]){  
-int temp, array[]={1, 2, 3, 6, 3, 9};  
-for (int i = 0; i < 5; i++){  
+    Scanner scan = new Scanner(System.in);
+    int limit, i, temp;
+    int array[] = new int[10];
+    
+    System.out.println("Enter array limit: ");
+    limit = scan.nextInt();
+
+    System.out.println("Enter array limit: ");
+    for(i = 0; i < limit; i++)
+       array[i] = scan.nextInt();
+
+    scan.close();
+
+    for (i = 0; i < limit; i++){  
                 if (array[i] > array[i + 1]){  
                     
                     temp = array[i];  
