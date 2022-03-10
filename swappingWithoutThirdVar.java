fb438602f7d14ded1745660bba740cb5fcf5dfe8
004Ajay <85185407+004Ajay@users.noreply.github.com>
2022-03-10 21:40:20 +0530

    swapping added

--- swappingWithoutThirdVar.java
+++ swappingWithoutThirdVar.java
@@ -1,8 +1,7 @@
-	import java.util.Scanner;
-	public class swapping {
-
+import java.util.Scanner;
+public class swappingWithoutThirdVar{
 		public static void main(String[] args) {
-			int num1, num2, temp;
+			int num1, num2;
 			Scanner scan=new Scanner(System.in);
 			System.out.print("Enter value for num1 & num2:  " );
 			num1 = scan.nextInt();
@@ -10,14 +9,13 @@
 			
 			scan.close();
 
-			// Java Program to swap two numbers using third variable
-			temp = num1;
-			num1 = num2;
-			num2 = temp;
+            num1 = num1 + num2;
+			num2 = num1 - num2;
+			num1 = num1 - num2;
 			
 			System.out.println("");
-			System.out.println("Swapping using third variable 'temp'");
+			System.out.println("Swapping without using third variable 'temp'");
 			System.out.println("Before swapping num1: " + num2 + " & num2:  "+num1);
 			System.out.println("After swapping num1: " + num1 + " & num2:  "+num2);
-		}
-	}
+    }
+}
